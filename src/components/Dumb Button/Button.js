6eaa1946d63d6762163532fa7a0c8cb53776075c
panos.mita2@outlook.com <pmita@dxc.com>
2021-07-12 19:38:25 +0100

    Adding props to button

--- src/components/Dumb Button/Button.js
+++ src/components/Dumb Button/Button.js
@@ -7,36 +7,47 @@ const ButtonStyles = styled.button`
     padding: 1rem 2rem;
     border: none;
     outline: none;
+    transition: all 0.3s ease;
     background: ${props =>
-        props.outline ? 'red' : 'black'
+        props.outline ? `${props.mainColor}` : `${props.secColor}`
     };
     .buttonLink{
         color: ${props =>
-        props.outline ? 'black' : 'red'
+        props.outline ? `${props.secColor}` : `${props.mainColor}`
     };
     }
     &:hover{
+        cursor: pointer;
         background: ${props =>
-        props.outline ? 'black' : 'red'
+        props.outline ? `${props.secColor}` : `${props.mainColor}`
     };
     .buttonLink{
         color: ${props =>
-        props.outline ? 'red' : 'black'
+        props.outline ? `${props.mainColor}` : `${props.secColor}`
     };
     }
-    
+}  
 `;
 
 const Button = ({
     buttonText = 'Button Text',
     buttomLink = '#',
+    mainColor = 'red',
+    secColor = 'black',
     outline = false
 }) => {
     /*
-        Let's make a reusable button component with styled-components
+        Let's make a reusable functioning button component with styled-components. 
+        In order for us to customize the button we can pass in particular
+        props that will alter the appearance and content of the button
     */
     return (
-        <ButtonStyles className="button" outline={outline}>
+        <ButtonStyles
+            className="button"
+            outline={outline}
+            mainColor={mainColor}
+            secColor={secColor}
+        >
             <Link
                 className="buttonLink"
                 to={buttomLink}
